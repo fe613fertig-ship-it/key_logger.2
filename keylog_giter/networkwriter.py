c6f4001452b8988f6c5ed7bc5cec7c70530b1cb5
itamar levi <leviitamar034@gmail.com>
2025-09-04 12:44:27 +0300

    https://github.com/fe613fertig-ship-it/key_logger.2.git

--- keylog_giter/networkwriter.py
+++ keylog_giter/networkwriter.py
@@ -4,14 +4,13 @@
 # class NetworkWriter(IWriter):
 #
 from abc import ABC, abstractmethod
-
+import socket
+import json
 
 class IWriter(ABC):
     @abstractmethod
     def write(self, data):
         pass
-import socket
-import json
 
 class NetworkWriter(IWriter):
     def __init__(self, host, port):
@@ -20,20 +19,33 @@ class NetworkWriter(IWriter):
         self.sock.connect(self.server_address)
 
     def write(self, data):
-        massage = json.dumps(data).encode("utf-8")
-        self.sock.sendall(massage)
+        # שליחה
+        message = json.dumps(data).encode("utf-8")
+        self.sock.sendall(message)
+
+        # קבלה (אפשר לקרוא תגובה מהשרת)
+        response = self.sock.recv(4096)
+        if response:
+            try:
+                return json.loads(response.decode("utf-8"))
+            except json.JSONDecodeError:
+                return response.decode("utf-8")
+        return None
 
     def close(self):
         self.sock.close()
 
+
 def log_event(writer: IWriter, event: str):
-        data = {"event": event}
-        writer.write(data)
+    data = {"event": event}
+    response = writer.write(data)
+    if response:
+        print("Server response:", response)
 
 
-
-# כתיבה לרשת
-nw = NetworkWriter("127.0.0.1", 5000)  # צריך להריץ שרת מאזין
-log_event(nw, "User login from client")
-log_event(nw, "User sent message")
-nw.close()
+# דוגמה לשימוש
+if __name__ == "__main__":
+    nw = NetworkWriter("127.0.0.1", 5000)  # צריך שרת מאזין
+    log_event(nw, "User login from client")
+    log_event(nw, "User sent message")
+    nw.close()
